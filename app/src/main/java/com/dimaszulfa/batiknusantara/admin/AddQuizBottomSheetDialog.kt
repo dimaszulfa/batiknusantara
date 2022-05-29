e18749aa8ed3761857fe294f99c06f8c8276e5f6
dimaszulfa <dimaszulfa0@gmail.com>
2022-05-30 06:50:03 +0700

    custom bottom sheet dialog for add answer

--- app/src/main/java/com/dimaszulfa/batiknusantara/admin/AddQuizBottomSheetDialog.kt
+++ app/src/main/java/com/dimaszulfa/batiknusantara/admin/AddQuizBottomSheetDialog.kt
@@ -1,9 +1,13 @@
 package com.dimaszulfa.batiknusantara.admin
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.dimaszulfa.batiknusantara.R
 import com.dimaszulfa.batiknusantara.databinding.FragmentAddQuizBottomSheetDialogBinding
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
@@ -29,13 +33,40 @@ class AddQuizBottomSheetDialog : BottomSheetDialogFragment() {
             binding.etOptionTwo.isEnabled = false
             binding.etOptionThree.isEnabled = false
             binding.etOptionFour.isEnabled = false
-
-            binding.etOptionFour.setOnClickListener {
-                binding.etOptionFour.setBackgroundColor(resources.getColor(R.color.colorPrimary))
-            }
+            selectTheCorrectAnswer()
         }
 
 
+    }
+
+    private fun selectTheCorrectAnswer() {
+        AlertDialog.Builder(requireContext())
+            .setTitle("Judul")
+            .setMessage("Pesan yang tampil")
+            .setPositiveButton("Ya", DialogInterface.OnClickListener { dialogInterface, i ->
+                Toast.makeText(requireContext(), "Jalankan perintah ketika user memilih tombol Ya", Toast.LENGTH_LONG).show()
+                binding.spCorrect.visibility = View.VISIBLE
+                with(binding){
+                    val op1 = binding.etOptionOne.text.toString()
+                    val op2 = binding.etOptionTwo.text.toString()
+                    val op3 = binding.etOptionThree.text.toString()
+                    val op4 = binding.etOptionFour.text.toString()
+                    val dataOptions = arrayListOf<String>(op1,op2,op3,op4)
+                    val aa = ArrayAdapter(requireContext(), androidx.transition.R.layout.support_simple_spinner_dropdown_item, dataOptions)
+                    aa.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                    spCorrect!!.adapter = aa
+                }
+            })
+            .setNegativeButton("No", DialogInterface.OnClickListener { dialogInterface, i ->
+                Toast.makeText(requireContext(), "Jalankan perintah ketika user memilih tombol No", Toast.LENGTH_LONG).show()
+                binding.etQuestion.isEnabled = true
+                binding.etOptionOne.isEnabled = true
+                binding.etOptionTwo.isEnabled = true
+                binding.etOptionThree.isEnabled = true
+                binding.etOptionFour.isEnabled = true
+                binding.spCorrect.visibility = View.GONE
+            })
+            .show()
     }
 
 }
